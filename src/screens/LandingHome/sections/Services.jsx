559b96cf9f758e6a652ce3451cc6759faf317ce2
MarawanAbdelaziz <marawan.abdelaziz33@gmail.com>
2024-12-24 18:00:01 +0200

    last

--- src/screens/LandingHome/sections/Services.jsx
+++ src/screens/LandingHome/sections/Services.jsx
@@ -16,11 +16,11 @@ const Services = () => {
 
       <div dir="rtl" className="flex mt-20 w-[75%] mx-auto">
         <div className="border-t border-l border-b basis-1/3 mx-5">
-          <div className="pt-10 pb-44 ">
+          <div className="pt-10 pb-44">
             <div className="w-full">
-              <img className="mx-auto w-16" src={g1538} alt="" />
+              <img className="mx-auto w-16" src={g1538} alt="g1538" />
             </div>
-            <h3 className="text-nowrap font-cairo font-semibold text-lg text-orange text-center mr-12 mt-6">
+            <h3 className="text-nowrap font-cairo font-semibold text-base 2xl:text-lg text-orange text-center mr-12 mt-6">
               وظائف متاحة
             </h3>
             <p className="font-cairo text-xs text-center text-gray1 mt-6">
@@ -30,11 +30,11 @@ const Services = () => {
           </div>
         </div>
         <div className="border-t border-l border-b basis-1/3 mx-5">
-          <div className="pt-10 pb-44 ">
+          <div className="pt-10 pb-44">
             <div className="w-full">
-              <img className="mx-auto w-16" src={g1539} alt="" />
+              <img className="mx-auto w-16" src={g1539} alt="g1539" />
             </div>
-            <h3 className="text-nowrap font-cairo font-semibold text-lg text-orange text-center mt-6">
+            <h3 className="text-nowrap font-cairo font-semibold text-base 2xl:text-lg text-orange text-center mt-6">
               آخر الأخبار
             </h3>
             <p className="font-cairo text-xs text-gray1 mt-6 text-center ml-10">
@@ -44,11 +44,11 @@ const Services = () => {
           </div>
         </div>
         <div className="border-t border-l border-b basis-1/3 mx-5">
-          <div className="pt-10 pb-44 ">
+          <div className="pt-10 pb-44">
             <div className="w-full">
               <img className="mx-auto w-16" src={g1535} alt="g1535" />
             </div>
-            <h3 className="text-nowrap font-cairo font-semibold text-lg text-orange text-center mr-20 mt-6">
+            <h3 className="text-nowrap font-cairo font-semibold text-base 2xl:text-lg text-orange text-center mr-20 mt-6">
               صيانة الأجهزة الكهربائية
             </h3>
             <p className="font-cairo text-xs text-center text-gray1 mt-6">
